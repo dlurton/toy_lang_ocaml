96f66cf92551bc26863cf9970cb05516da21715e
David Lurton <dlurton@gmail.com>
2018-12-02 16:06:08 -0800

    Basic REPL complete but doesn't handle syntax errors gracefully yet

--- src/repl.ml
+++ src/repl.ml
@@ -2,25 +2,21 @@
 open Ast
 open Interp;;
 
-
-let line_stream_of_channel channel =
-    Stream.from
-      (fun _ ->
-         try Some (input_line channel) with End_of_file -> None);
-
-let main =
-    print_string("OCAML toy language REPL - type \"?exit\" to exit"); 
-    let rec eval_line line_stream =
-      match Stream.next(line_stream) with
-      | None -> ()
-      | Some line_text ->
-        (match line_text with
-        | "?exit" -> ()
-        | _ ->
-          let value = interp(line_text) in
-          let value_text = string_of_int(value) in
-          print_string(value_text);
-          eval_line(line_stream)
-        )
-    in let stdin_line_stream = line_stream_of_channel(stdin) in
-           eval_line(stdin_line_stream)
+(* main entry point *)
+print_endline("OCAML toy language REPL - type \"?exit\" to exit");
+let line_reader = fun () -> try Some (input_line stdin) with End_of_file -> None in
+let rec interpret_line () =
+  print_string(">");
+  flush(stdout);
+  match line_reader() with
+  | None -> ()
+  | Some line_text ->
+    match line_text with
+    | "?exit" -> ()
+    | _ ->
+      let value = interp(line_text) in
+      let value_text = string_of_int(value) in
+      print_endline (value_text);
+      interpret_line();
+in
+  interpret_line()

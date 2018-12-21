5adaa936154e9a09f28719c181db22960b485653
David Lurton <dlurton@amazon.com>
2018-12-20 18:23:44 -0800

    linenoise WIP

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ Prerequisites:
 - `opam init`
 - `opam setup -a`
 - `opam install menhir`
-- 'opam install dune'
+- `opam install dune`
 
-To build, run `dune build` from the project's root directory.  To test, build and then run `dune runtest`.
+To build, run `dune build` from the project's root directory.  To build and test, run `dune runtest`.
 
 I also recommend:
 
@@ -31,4 +31,7 @@ Tested with `ocamlc` 4.07.1.
 - [OUnit Documentation](http://ounit.forge.ocamlcore.org/api-ounit/index.html)
 - [OCaml Stack Overflow](https://stackoverflow.com/questions/tagged/ocaml)i
 - [Menhir reference manual](http://gallium.inria.fr/~fpottier/menhir/manual.pdf)
-- [Lexing module documentation: https://caml.inria.fr/pub/docs/manual-ocaml/libref/Lexing.html]
+- [Lexing module documentation](https://caml.inria.fr/pub/docs/manual-ocaml/libref/Lexing.html)
+- [opam packaging](https://opam.ocaml.org/doc/Packaging.html) (Describies files like `toy_lang.opam`)
+- [Starting a new project with dune](https://medium.com/@bobbypriambodo/starting-an-ocaml-app-project-using-dune-d4f74e291de8)
+
--- bin/repl.ml
+++ bin/repl.ml
@@ -4,38 +4,45 @@ open Toy_lang.Core
 
 ;;
 
-(* program entry point *)
-print_endline("OCAML toy language REPL - type \"?exit\" to exit");
+let rec user_input prompt cb =
+  match LNoise.linenoise prompt with
+  | None -> ()
+  | Some v ->
+    cb v;
+    user_input prompt cb
 
 let show_error sloc msg =
   if (String.length sloc.file) != 0 then Printf.printf "%s: " sloc.file;
   Printf.printf "(%d, %d): %s\n" sloc.line_num sloc.char_offset msg;
   flush(stdout)
-in
 
-let line_reader = fun () -> try Some (input_line stdin) with End_of_file -> None in
-let rec interpret_line () =
-  print_string(">");
-  flush(stdout);
-  begin
-    match line_reader() with
-    | None -> exit(0)
-    | Some line_text ->
-      match line_text with
-      | "?exit" -> exit(0)
-      | _ ->
-        (* attempt to parse the line *)
-        let presult = parse(line_text) in
-        match presult with
-        | ParseError(sloc, pmsg) -> show_error sloc pmsg
-        | ParseSuccess e ->
-          (* attempt to interpret the AST *)
-          let iresult = eval_with_empty_env e in
-          match iresult with
-          | InterpError(sloc, imsg) -> show_error sloc imsg
-          | InterpSuccess value ->
-            print_endline(value_to_string(value))
+let interpret_line line_text =
+  (* attempt to parse the line *)
+  let presult = parse(line_text) in
+  match presult with
+  | ParseError(sloc, pmsg) -> show_error sloc pmsg
+  | ParseSuccess e ->
+    (* attempt to interpret the AST *)
+    let iresult = eval_with_empty_env e in
+    match iresult with
+    | InterpError(sloc, imsg) -> show_error sloc imsg
+    | InterpSuccess value ->
+      print_endline(value_to_string(value))
+
+let () =
+  (* LNoise.set_multiline true; *)
+  LNoise.history_load ~filename:"toy_lang.history" |> ignore;
+  LNoise.history_set ~max_length:1000 |> ignore;
+  LNoise.set_completion_callback begin fun line_so_far ln_completions ->
+    if line_so_far <> "" && line_so_far.[0] = 'h' then
+      ["if"; "func";]
+      |> List.iter (LNoise.add_completion ln_completions);
   end;
-  interpret_line()
-in
-  interpret_line();
+  print_endline("OCAML toy language REPL - type \"?exit\" to exit");
+  (fun from_user ->
+     if from_user = "?exit" then exit 0;
+     LNoise.history_add from_user |> ignore;
+     LNoise.history_save ~filename:"history.txt" |> ignore;
+     Printf.sprintf "Got: %s" from_user |> interpret_line
+  )
+  |> user_input "> "
